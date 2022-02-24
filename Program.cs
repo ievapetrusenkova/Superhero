260dd9bfafa2e451036858fea14bdbb609f7cf89
ievapetrusenkova <ieva.pilenge@gmail.com>
2022-02-24 21:13:28 +0200

    Added variables Lesson3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "SuperSonic";
+int age = 77;
+string HeroPower1, HeroPower2, HeroPower3;
+HeroPower1 = "runs superfast";
+HeroPower2 = "invulnerability";
+HeroPower3 = "light speed spin dash";
+
+
+Console.WriteLine("=============");
+Console.WriteLine(" Hero");
+Console.WriteLine("===============");
+Console.WriteLine($"Hero name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"HeroPowers: \n {HeroPower1}, \n {HeroPower2}, \n {HeroPower3}");
+Console.WriteLine("==============");
+
+
+
+
+
